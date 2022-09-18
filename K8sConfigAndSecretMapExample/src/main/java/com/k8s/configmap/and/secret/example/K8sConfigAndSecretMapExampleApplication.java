3e70ff9957e693c3230a18e7243104164b912704
Naresh Makhija <makhijanaresh@hotmail.com>
2022-09-18 15:34:03 +0530

    K8s ConfigMap & Secret

---
+++ K8sConfigAndSecretMapExample/src/main/java/com/k8s/configmap/and/secret/example/K8sConfigAndSecretMapExampleApplication.java
@@ -0,0 +1,13 @@
+package com.k8s.configmap.and.secret.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sConfigAndSecretMapExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sConfigAndSecretMapExampleApplication.class, args);
+	}
+
+}
---
+++ K8sConfigAndSecretMapExample/src/test/java/com/k8s/configmap/and/secret/example/K8sConfigAndSecretMapExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.configmap.and.secret.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sConfigAndSecretMapExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
