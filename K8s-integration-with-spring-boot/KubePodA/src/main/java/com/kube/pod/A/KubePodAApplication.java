fd29f6ac3ef174e3da90bee51d5db7a0d76d6ec5
Naresh Makhija <makhijanaresh@hotmail.com>
2026-04-03 21:17:46 +0530

    Initial commit

---
+++ K8s-integration-with-spring-boot/KubePodA/src/main/java/com/kube/pod/A/KubePodAApplication.java
@@ -0,0 +1,13 @@
+package com.kube.pod.A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubePodAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubePodAApplication.class, args);
+	}
+
+}
---
+++ K8s-integration-with-spring-boot/KubePodA/src/test/java/com/kube/pod/A/KubePodAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kube.pod.A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubePodAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
