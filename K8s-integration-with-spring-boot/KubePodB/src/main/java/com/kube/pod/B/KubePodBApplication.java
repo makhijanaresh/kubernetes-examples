09c529b53e73721f4a95396a3f8da8b2bcd815cf
Naresh Makhija <makhijanaresh@hotmail.com>
2026-04-03 21:20:59 +0530

    Intial for KubePodB

---
+++ K8s-integration-with-spring-boot/KubePodB/src/main/java/com/kube/pod/B/KubePodBApplication.java
@@ -0,0 +1,13 @@
+package com.kube.pod.B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubePodBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubePodBApplication.class, args);
+	}
+
+}
