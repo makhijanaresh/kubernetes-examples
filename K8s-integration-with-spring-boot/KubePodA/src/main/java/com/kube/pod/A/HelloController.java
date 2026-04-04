b99473909401c855d6ece0053f54a3f646975821
Naresh Makhija <makhijanaresh@hotmail.com>
2026-04-04 13:06:22 +0530

    Code for KubePodA

---
+++ K8s-integration-with-spring-boot/KubePodA/src/main/java/com/kube/pod/A/HelloController.java
@@ -0,0 +1,13 @@
+package com.kube.pod.A;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello from PodA";
+	}
+}
